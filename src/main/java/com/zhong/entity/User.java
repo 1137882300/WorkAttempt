5e133a9ba4f3556d4cb7bf8359427b0bd65cc69b
zhongzihan <923828430@qq.com>
2021-10-09 16:05:31 +0800

    10.9

--- src/main/java/com/zhong/entity/User.java
+++ src/main/java/com/zhong/entity/User.java
@@ -1,7 +1,9 @@
 package com.zhong.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 /**
@@ -10,10 +12,12 @@ import lombok.experimental.SuperBuilder;
  */
 
 @Data
-@SuperBuilder(toBuilder = true)
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class User extends People{
 
+    private int id;
     private int age;
 
-    private int id;
 }
--- src/test/java/com/zhong/LambdaTest.java
+++ src/test/java/com/zhong/LambdaTest.java
@@ -2,11 +2,15 @@ package com.zhong;
 
 import com.google.common.collect.Lists;
 import com.zhong.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -15,6 +19,22 @@ import java.util.stream.Collectors;
  */
 public class LambdaTest {
 
+    private User u1,u2,u3,u4,u5,u6,u7;
+
+    @Before
+    public void init(){
+        u1 = User.builder().id(1).age(4).build();
+        u2 = User.builder().id(2).age(34).build();
+        u3 = User.builder().id(3).age(15).build();
+        u4 = User.builder().id(4).age(52).build();
+        u5 = User.builder().id(5).age(23).build();
+        u6 = User.builder().id(6).age(51).build();
+    }
+
+    /**
+     * groupingBy
+     * mapping
+     */
     @Test
     public void test1(){
         User u1 = User.builder().age(1).id(1).build();
@@ -32,4 +52,34 @@ public class LambdaTest {
         User user = User.builder().id(1).build();
     }
 
+    /**
+     * Optional
+     */
+    @Test
+    public void test3(){
+        List<User> users = Lists.newArrayList(u1, u2, u3, u4,u5,u6,u7);
+        Optional.of(users).orElse(Collections.emptyList()).forEach(System.out::println);
+        users.forEach(System.out::println);
+
+        Optional<User> optional = Optional.ofNullable(User.builder().build());
+        if (optional.isPresent()){
+            String s = optional.get().toString();
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * toBuilder
+     */
+    @Test
+    public void test4(){
+        User user = User.builder().age(1).id(1).build();
+        System.out.println(user);
+        User build = user.toBuilder().id(2).age(2).build();
+        System.out.println(build);
+        System.out.println(user == build);
+        System.out.println(user.equals(build));
+    }
+
 }
+
