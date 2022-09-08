0cacbeec829b1e46872c2561bbc223c37092237d
zhongzihan <923828430@qq.com>
2022-09-08 09:40:53 +0800

    2022.9.7

--- src/test/java/com/zhong/CollectionTest.java
+++ src/test/java/com/zhong/CollectionTest.java
@@ -20,6 +20,16 @@ import java.util.stream.Collectors;
  */
 public class CollectionTest {
 
+    @Test
+    public void values() {
+        Map<String, List<String>> listMap = Maps.newHashMap();
+        listMap.put("aa", Lists.newArrayList("11", "22"));
+        listMap.put("bb", Lists.newArrayList("11", "22"));
+        System.out.println(listMap.values());//[[11, 22], [11, 22]]
+        List<String> collect = listMap.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
+        System.out.println(collect);//[11, 22, 11, 22]
+    }
+
     /**
      *
      */
