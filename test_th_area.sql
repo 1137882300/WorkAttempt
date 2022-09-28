3a7d0b265d5975958044013eb37680ebb6b4123d
zhongzihan <923828430@qq.com>
2022-09-28 16:57:39 +0800

    2022.9.28

--- test_th_area.sql
+++ test_th_area.sql
@@ -1,63 +1,62 @@
--- cspu的：
--- cspuID：20000039827  code：4547691689696  type：'CSPU_INTERNATIONAL'        2000000494
--- cspuID：20000043125  code：4547691689778	 type：'CSPU_INTERNATIONAL'          2000000494
--- cspuID：20000043144 	code：4547691689696  type：'CSPU_INTERNATIONAL'           2000000494
-
-
--- sku的：
--- skuID：29827  code：4547691689696   type = 'SKU_INTERNATIONAL'     itemID：13633
--- skuID：33144  code：4547691689696   type = 'SKU_INTERNATIONAL'        itemID：15480
--- skuID：20000007037  code：4547691784865  type = 'SKU_INTERNATIONAL'        itemID：2000005637
--- skuID：20000007167  code：4547691239099  type = 'SKU_INTERNATIONAL'    itemID：2000005739
-
-
-insert into `t_global_relation` (`create_time`, `creator_id`, `features`, `id`, `is_deleted`, `master`, `modified_id`,
-                                 `platform`, `slave`, `status`, `system_update_time`, `type`, `update_time`, `version`)
-values ('1663745141000', '-1', '{"shopId":"2000000494"}', '2000001250', 0, '20000039827', '-1', NULL, '4547691689696',
-        0, NULL, 'CSPU_INTERNATIONAL', '1663745141000', '1');
-insert into `t_global_relation` (`create_time`, `creator_id`, `features`, `id`, `is_deleted`, `master`, `modified_id`,
-                                 `platform`, `slave`, `status`, `system_update_time`, `type`, `update_time`, `version`)
-values ('1663745141000', '-1', '{"shopId":"2000000494"}', '2000001249', 0, '20000043125', '-1', NULL, '4547691689778',
-        0, NULL, 'CSPU_INTERNATIONAL', '1663745141000', '1');
-insert into `t_global_relation` (`create_time`, `creator_id`, `features`, `id`, `is_deleted`, `master`, `modified_id`,
-                                 `platform`, `slave`, `status`, `system_update_time`, `type`, `update_time`, `version`)
-values ('1663745141000', '-1', '{"shopId":"2000000494"}', '2000001248', 0, '20000043144', '-1', NULL, '4547691689696',
-        0, NULL, 'CSPU_INTERNATIONAL', '1663745141000', '1');
-
-insert into `t_global_relation` (`create_time`, `creator_id`, `features`, `id`, `is_deleted`, `master`, `modified_id`,
-                                 `platform`, `slave`, `status`, `system_update_time`, `type`, `update_time`, `version`)
-values ('1663745141000', '-1', '{"itemId":"13633","shopId":"2000000494"}', '2000001247', 0, '29827', '-1', NULL,
-        '4547691689696', 0, NULL, 'SKU_INTERNATIONAL', '1663745141000', '1');
-insert into `t_global_relation` (`create_time`, `creator_id`, `features`, `id`, `is_deleted`, `master`, `modified_id`,
-                                 `platform`, `slave`, `status`, `system_update_time`, `type`, `update_time`, `version`)
-values ('1663745141000', '-1', '{"itemId":"15480","shopId":"2000000494"}', '2000001246', 0, '33144', '-1', NULL,
-        '4547691689696', 0, NULL, 'SKU_INTERNATIONAL', '1663745141000', '1');
-insert into `t_global_relation` (`create_time`, `creator_id`, `features`, `id`, `is_deleted`, `master`, `modified_id`,
-                                 `platform`, `slave`, `status`, `system_update_time`, `type`, `update_time`, `version`)
-values ('1663745141000', '-1', '{"itemId":"2000005637","shopId":"2000000494"}', '2000001245', 0, '20000007037', '-1',
-        NULL, '4547691784865', 0, NULL, 'SKU_INTERNATIONAL', '1663745141000', '1');
-insert into `t_global_relation` (`create_time`, `creator_id`, `features`, `id`, `is_deleted`, `master`, `modified_id`,
-                                 `platform`, `slave`, `status`, `system_update_time`, `type`, `update_time`, `version`)
-values ('1663745141000', '-1', '{"itemId":"2000005739","shopId":"2000000494"}', '2000001244', 0, '20000007167', '-1',
-        NULL, '4547691239099', 0, NULL, 'SKU_INTERNATIONAL', '1663745141000', '1');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_brand_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_brand', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_brand_trademark', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_brand_type_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_brand_type', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_category_backend_brand_relation', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_category_backend_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_category_backend', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_property_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_property', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_trademark_rule_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_trademark_rule', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_goods_unit', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_goods_unit_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_country_locale', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_country_locale_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_tag_meta', 2000000000, 1010000000100, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_tax_rate', 2000000000, 1010000000001, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_global_relation', 2000000000, 1010000117020, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_category_relation', 2000000000, 1010000000080, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_property_value', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_property_value_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_currency', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_currency_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_area_info', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_area_info_language', 2000000000, 1010000000250, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_property_relation', 2000000000, 1010000000080, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_record_push', 2000000000, 1010000000460, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_tag_meta_language', 2000000000, 1010000000010, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('t_tag_relation', 2000000000, 1010000000010, 10, 'bbm');
+INSERT INTO `t_segment` (`biz_tag`, `init_val`, `current_val`, `step`, `tenant_id`)
+VALUES ('tag_code_generate', 2000000000, 1010000000010, 10, 'bbm');
