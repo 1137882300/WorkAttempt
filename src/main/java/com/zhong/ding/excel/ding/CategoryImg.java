e62a6235ffe9ef9df4e7a16530784a103ab1a7aa
zhongzihan <923828430@qq.com>
2022-11-03 17:49:46 +0800

    2022.11.3

---
+++ src/main/java/com/zhong/ding/excel/ding/CategoryImg.java
@@ -0,0 +1,44 @@
+package com.zhong.ding.excel.ding;
+
+import cn.hutool.core.io.FileUtil;
+import com.google.common.collect.Lists;
+import com.zhong.Utils.FileUtils;
+import com.zhong.ding.excel.Entity;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @date 2022/11/3 16:43
+ */
+public class CategoryImg {
+
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\EDZ\\Documents\\0订数据\\订正ＣＢＧ\\订正类目图片\\源文件\\源文件2.xlsx";
+        List<Entity> list = FileUtils.readExcel(path);
+
+        if (CollectionUtils.isEmpty(list)) {
+            return;
+        }
+
+        ArrayList<String> sql = Lists.newArrayList();
+        for (Entity entity : list) {
+            String categoryId = entity.getColumn1();
+            String img = entity.getColumn3();
+
+            sql.add(
+                    "update t_category_backend set version=version+1, update_time=1667465151000, icon_url= '" + img + "' where category_backend_id = " + categoryId + " ;"
+            );
+
+        }
+
+        FileUtil.writeUtf8Lines(sql, new File("category.sql"));
+
+    }
+
+
+}
