be01f1c2446cb6bfa499c4db8a78a51108442fca
zzh <zzh@youxiake.com>
2023-04-14 11:04:13 +0800

    2023-4-14

---
+++ src/main/java/com/zhong/chatgpt/Test.java
@@ -0,0 +1,26 @@
+package com.zhong.chatgpt;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zhong.network.HttpService;
+import javafx.util.Pair;
+
+/**
+ * @author: juzi
+ * @date: 2023/4/14
+ * @desc:
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        // 构造请求体，JSON格式，包含一个字符串参数prompt和一个整数参数max_tokens，如果有其他参数，延续即可。
+        String json = "{\"model\": \"text-davinci-003\", \"prompt\": \"Hello, ChatGPT!\", \"max_tokens\": 1000}";
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        Pair<String, String> header = new Pair<>("Authorization", "Bearer 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");
+        HttpService.post("https://api.openai.com/v1/chat/completions", header, jsonObject);
+
+    }
+
+
+}
--- src/main/java/com/zhong/network/HttpService.java
+++ src/main/java/com/zhong/network/HttpService.java
@@ -1,7 +1,11 @@
 package com.zhong.network;
 
 import com.alibaba.fastjson.JSONObject;
+import javafx.util.Pair;
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
 import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
 import org.apache.http.util.EntityUtils;
@@ -23,6 +27,16 @@ public class HttpService {
                 .addHeader("Content-Type", "application/json;  charset=utf-8");
         return doRequest(request);
     }
+
+
+    public static JSONObject post(String path, Pair<String,String> header , JSONObject body) {
+        Request request = Request.Post(path)
+                .addHeader("Content-Type", "application/json;  charset=utf-8")
+                .addHeader(header.getKey(), header.getValue())
+                .bodyString(body.toJSONString(), ContentType.APPLICATION_JSON);
+        return doRequest(request);
+    }
+
 
     private static JSONObject doRequest(Request request) {
         try {
