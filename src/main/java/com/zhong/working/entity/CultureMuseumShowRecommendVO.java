9755f93059d51c004f57ecde1d68f36cecc85f97
zzh <zzh@youxiake.com>
2023-04-27 18:21:03 +0800

    2023-4-27

---
+++ src/main/java/com/zhong/working/entity/CultureMuseumShowRecommendVO.java
@@ -0,0 +1,14 @@
+package com.zhong.working.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author: 竹阳
+ * @date: 2022-12-06
+ **/
+@Data
+public class CultureMuseumShowRecommendVO {
+    private List<CultureCalendarActivityVO.DayActivityVO> dataList;
+}
