5c462b1ef691ae755405cba7821205d977d233ef
zzh <zzh@youxiake.com>
2023-06-13 15:49:03 +0800

    2023-6-13

--- src/main/java/com/zhong/decryption/Test.java
+++ src/main/java/com/zhong/decryption/Test.java
@@ -1,5 +1,6 @@
 package com.zhong.decryption;
 
+import cn.hutool.crypto.SecureUtil;
 import com.zhong.cache.Main;
 import lombok.SneakyThrows;
 
@@ -23,9 +24,13 @@ public class Test {
      * @param args
      */
 
-
-    @SneakyThrows
     public static void main(String[] args) {
+        String s = SecureUtil.md5("123456");
+        System.out.println(s
+        );
+    }
+    @SneakyThrows
+    public static void main1(String[] args) {
         String encryptedData = "+2IEjaY+hWHm1fxlwz0fWED3H6+3ngguEBLxTwaPBlqaj8F8DZRMgW1f37UyQajxx8Z+guAS0WIDUa9XDvKmF+z+K90FfZ0xWblLZ2WVUdaQlS1GCopX1sC7ckVVD5lLIuaBWSdPFXOQij6IAYJXIaJ+i+yp/YO9q0bf4UPkEpFW42yxcCUkS7hE+1lCapmB5BCFdNHQAHXM34LftfwBEAqP2kdUbcW5cfumPEeBZETXC3eWhEr7JTsHWxT6d7LmftzsfuYSXe/UZClfyA/QWUrJzxonZ975+E45At0ud4M9vYefnHDczEh8E4pKFMKl4u5GzzhQE9vSJxVb4l7Ss2mB7dVdiwGb9pSdz8Z8mOSQMIJfZEqh5dVWmZc4dzwZ51caCVlEvVIkKz+0MzVaWVVpmYAuQoLB+/x7VjSL+Mi3BoABm0hvg9l/YyuEsozBme0vYiGdP9w7i/C8+P0MWhN+Ruq6PHSM31Fvy72ITeLWN2+Li3h7zARoZ99dduIWzy9mFMmY7QIjchKg+ctQuDfmgYp+qI9ed/UQ8AZAPfRzHfg4Zsv/HxSD55ujUkQRpVF5VeY7xu75jDacirk+otUEyv8ppa5VeVWJPHvAfF+bzJ7t8mN8hzhAe2y9ctFkfFXeaizKHHnafkieP4yzeajt6Wtf5ilrbyVfqpldAp69bfz5jcmbhAwuqUFD6rVT98/diM5E2PM2svdBJyxQmZLkWQjUOS+a51Z+/5lkDn3c6OLFfG+WZWbMLcajmk0m8nO51PX+0NcDPsmx3PUdbu9UgQ/W9fhZD5p7hpnINn7HocD7oD1E0/Vt4PYT4WMgRXNJCQ8dsJdETe+VvWbxh8YNxkJ5wRS5KKyq8V3aEAvgHCqmHFk2OnW0/sdKz3xX5Kr2gGC6fHUqMDbQhIUQ6+LCTHkbYPaonaWd6z1k/JW/miy6pitlNAqn0PecC3dBgL0XW0GbpHrZY7HN3j3HTKvUXDNrZQn9qhny9aR/4ryBaf0sfqeZ5/FwKsAVhZB/iXX2IWpR4OK/bIF7efmKfP+gWPXhTx629AlT50l5DA5C02TItHj121UIYE94Uu7omPMO5/BLsD2YPDcjnwWqFsPY45y3b9lDXcjH6IHlyFiVPkaMpFde6+SNnf+XvmgAulntHuMs4ge6rbvS+8pXLo3PgiJraEICjlrXl/f49XLJXW/Fd6YnOT05qhlwuXYR4f9CYmV+xLXcJHRhBicv87UmyKy3SxHuniBKxr0j/q9kBJPB2zCiXSsnThKGQXTLb1OQFN+1YGo1MxGcjNU5tiMB9Rdt/wnXBL7OcLZrlxfDr6+RFXbL68byBRqZJyum4WL/Sv0ME1mRQMWvuS65hi0ZdgKpXJOSl76Op6STTJpZF3q4ToGIrzHq63oLQaUCp2mYjZzc6ihy5JAa5xsEMr28pKUmcRf9rFO1xrLb+E6cXxkEvVLuAQa1xbjuN6zPIDLO1qIUPMKQFyRpUvRDhZGBu6IbA9Drba9Wu5w2QzZv4zDnHNxAgp9Tx63y9b2NGGzxYGJdgPfVdlDOJcYBfgXlRRwWHvIt29/rynq2hd14BrVCh9fu+4ClOVTQCXyqwlztgNVD0DJbeqUBbMove81kaPmFc+BZ54W0mqQzdoo=";
         String iv = "hg5LTVD2X3Hn91w6RB6/XQ==";
         String sessionKey = "kM9fdtiMgPfayqSzeg6rDg==";
