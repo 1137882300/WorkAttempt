52ad165ec9f36aae32e5896c43f6e3711eecc859
zzh <zzh@youxiake.com>
2024-01-15 09:33:34 +0800

    PostcardTest

--- src/main/java/com/zhong/working/youxiake/PostcardTest.java
+++ src/main/java/com/zhong/working/youxiake/PostcardTest.java
@@ -39,18 +39,25 @@ public class PostcardTest {
         public static void main(String[] args) {
             List<String> imageUrls = new ArrayList<>();
 
-            String url = "http://192.168.200.109:8000/api/postcard/preview?id=";
+            String ids = "137,136,135,134,133,132,131,130,129,128,127,126,125,124,123,122,121,120,119,118,117,116,115,114,113,112,111,110,109,108,107,106,105,104,103,102,101,100,99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8";
+
+            String[] split = ids.split(",");
+
+//            String url = "http://192.168.200.109:8000/api/postcard/preview?id=";
+            String url = "https://fxhz.wgly.hangzhou.gov.cn/dashboard/api/postcard/preview?id=";
 
             Map<String, String> header = Maps.newHashMap();
-            header.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4IiwiaWF0IjoxNzA0OTYyMjQ1LCJleHAiOjE3MDc1NTQyNDUsIm5iZiI6MTcwNDk2MjI0NX0.HHFlP4hZ0D8GweNnKMXA34BuDOcpjST0bMk11mVD370");
+            header.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyMSIsImlhdCI6MTcwNDg3MTczNiwiZXhwIjoxNzA3NDYzNzM2LCJuYmYiOjE3MDQ4NzE3MzZ9._namY6UlmDaKDWq1uVQBD3CjKZXxT-XtxvsJ9Llo9pU");
 
-            for (int i = 0; i < 2; i++) {
-                JSONObject jsonObject = HttpService.get(url + 283, header);
+            for (String id : split) {
+                JSONObject jsonObject = HttpService.get(url + id, header);
                 JSONObject data = jsonObject.getJSONObject("data");
                 String imgUrl = data.getString("url");
                 System.out.println(imgUrl);
                 imageUrls.add(imgUrl);
             }
+
+            System.out.println("数量：" + imageUrls.size());
 
             ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
 
